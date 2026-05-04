4c97f09a0281b85f356e17a5fb25772d6d415578
DarkStarBen <gall.27ben@gmail.com>
2026-05-04 20:09:10 +0200

    Update c.c
    Most important update of the whole project PLS ACCEPT

--- c.c
+++ c.c
@@ -1,1 +1,15 @@
 //Hello!
+//This file is called c.c
+//c.c look like a face!
+
+//   CCCCCCC                  CCCCCCC
+//  C       C                C       C
+// C         C              C         C
+//C           C            C           C
+//C                        C
+//C                        C
+//C                        C
+//C                        C
+// C         C              C         C
+//  C       C                C       C
+//   CCCCCCC         .        CCCCCCC
