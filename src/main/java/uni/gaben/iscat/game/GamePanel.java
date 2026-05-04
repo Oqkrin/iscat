9615dc8898bda052f56c87b35d01e25afd592618
DarkStarBen <gall.27ben@gmail.com>
2026-05-04 19:04:51 +0200

    Migliorie alla classe Player e Entity.

--- src/main/java/uni/gaben/iscat/game/GamePanel.java
+++ src/main/java/uni/gaben/iscat/game/GamePanel.java
@@ -1,11 +1,14 @@
 package uni.gaben.iscat.game;
 
+import uni.gaben.iscat.game.entities.Player;
+
 import javax.swing.JPanel;
 import java.awt.*;
 
 public class GamePanel extends JPanel implements Runnable{
     // Impostazioni dello schermo
     final int scale = 3;
+    public final int tileSize = 32 * scale;
     final int screenWidth = 500;
     final int screenHeight = 500;
 
@@ -15,11 +18,7 @@ public class GamePanel extends JPanel implements Runnable{
     // oggetto keylistener
     KeyHandler keyHandler = new KeyHandler();
     Thread gameThread;
-
-    // Posizione iniziale del player
-    int player_x = 100;
-    int player_y = 100;
-    int player_speed = 4;
+    Player player = new Player(this,keyHandler);
 
     public GamePanel(){
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
@@ -38,66 +37,55 @@ public class GamePanel extends JPanel implements Runnable{
     // in base agli FPS diciamo al gioco di update() e repaint()
     @Override
     public void run() {
-            // 1.000.000.000 in nanosencondi è un secondo
-            double drawInterval = 1000000000/FPS; // 0.01666 secondi quindi disegna FPS volte al secondo
-            double delta = 0;
-            long lastTime = System.nanoTime();
-            long currentTime;
-            long timer = 0;
-            int drawCount = 0;
+        // 1.000.000.000 in nanosencondi è un secondo
+        double drawInterval = 1000000000/FPS; // 0.01666 secondi quindi disegna FPS volte al secondo
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+        long timer = 0;
+        int drawCount = 0;
 
-            while (gameThread != null) {
-                currentTime = System.nanoTime();
-                delta += (currentTime - lastTime) / drawInterval;
-                timer += (currentTime - lastTime);
-                lastTime = currentTime;
+        while (gameThread != null) {
+            currentTime = System.nanoTime();
+            delta += (currentTime - lastTime) / drawInterval;
+            timer += (currentTime - lastTime);
+            lastTime = currentTime;
 
-                if(delta >= 1) {
-                    update(); // aggiorna pos player
-                    repaint(); // disegna il player
-                    delta--;
-                    drawCount++;
-                }
-
-                if(timer >= 1000000000 && FPS_visible){
-                    System.out.print("FPS:"+drawCount);
-
-                    // >>>[--- DA ELIMINARE ---]<<<
-                    if(drawCount == 60){
-                        System.out.print(" YUPPI!!! :D");
-                        System.out.println(" ");
-                    } else {
-                        System.out.print(" NOOOOOOOO AGHAJGDHSGJADHK!!!");
-                        System.out.println(" ");
-                    }
-                    // una volta eliminata questa porzione di codice bisogna mettere println nel primo syso degli FPS
-                    // >>>[--- DA ELIMINARE ---]<<<
-                    drawCount = 0;
-                    timer = 0;
-                }
+            if(delta >= 1) {
+                update(); // aggiorna pos player
+                repaint(); // disegna il player
+                delta--;
+                drawCount++;
             }
+
+            if(timer >= 1000000000 && FPS_visible){
+                System.out.print("FPS:"+drawCount);
+
+                // >>>[--- DA ELIMINARE ---]<<<
+                if(drawCount == 60){
+                    System.out.print(" YUPPI!!! :D");
+                    System.out.println(" ");
+                } else {
+                    System.out.print(" NOOOOOOOO AGHAJGDHSGJADHK!!!");
+                    System.out.println(" ");
+                }
+                // una volta eliminata questa porzione di codice bisogna mettere println nel primo syso degli FPS
+                // >>>[--- DA ELIMINARE ---]<<<
+                drawCount = 0;
+                timer = 0;
+            }
+        }
     }
 
-    public void update() {
-        if(keyHandler.upPressed) {
-            player_y -= player_speed;
-        }
-        else if (keyHandler.downPressed) {
-            player_y += player_speed;
-        }
-        else if (keyHandler.rightPressed) {
-            player_x += player_speed;
-        }
-        else if (keyHandler.leftPressed) {
-            player_x -= player_speed;
-        }
+    public void update(){
+        player.update();
     }
+
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         Graphics2D g2 = (Graphics2D)g;
-        g2.setColor(Color.white);
-        g2.fillRect(player_x,player_y,50,50);
+        player.draw(g2);
         g2.dispose();
     }
 }
--- src/main/java/uni/gaben/iscat/game/entities/Player.java
+++ src/main/java/uni/gaben/iscat/game/entities/Player.java
@@ -1,4 +1,77 @@
 package uni.gaben.iscat.game.entities;
 
-public class Player {
+import uni.gaben.iscat.game.GamePanel;
+import uni.gaben.iscat.game.KeyHandler;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.Objects;
+
+public class Player extends Entity{
+    GamePanel gp;
+    KeyHandler keyHandler;
+
+    public Player(GamePanel gp, KeyHandler keyHandler){
+        this.gp = gp;
+        this.keyHandler = keyHandler;
+
+        setDefaultValues();
+        getPlayerSprites();
+    }
+
+    public void setDefaultValues(){
+        x = 100;
+        y = 100;
+        speed = 4;
+        direction = "up";
+    }
+
+    public void getPlayerSprites(){
+        try {
+            up1    = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/uni/gaben/iscat/sprites/battle_ship_1.png")));
+            up2    = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/uni/gaben/iscat/sprites/battle_ship_1.png")));
+            down1  = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/uni/gaben/iscat/sprites/battle_ship_1.png")));
+            down2  = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/uni/gaben/iscat/sprites/battle_ship_1.png")));
+            right1 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/uni/gaben/iscat/sprites/battle_ship_1.png")));
+            right2 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/uni/gaben/iscat/sprites/battle_ship_1.png")));
+            left1  = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/uni/gaben/iscat/sprites/battle_ship_1.png")));
+            left2  = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/uni/gaben/iscat/sprites/battle_ship_1.png")));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void update() {
+        if(keyHandler.upPressed) {
+            direction = "up";
+            y -= speed;
+        }
+        else if (keyHandler.downPressed) {
+            direction = "down";
+            y += speed;
+        }
+        else if (keyHandler.rightPressed) {
+            direction = "right";
+            x += speed;
+        }
+        else if (keyHandler.leftPressed) {
+            direction = "left";
+            x -= speed;
+        }
+    }
+
+    public void draw(Graphics2D g2){
+        //g2.setColor(Color.white);
+        //g2.fillRect(x,y,50,50);
+        BufferedImage image = switch (direction) {
+            case "up" -> up1;
+            case "down" -> down1;
+            case "right" -> right1;
+            case "left" -> left1;
+            default -> null;
+        };
+        g2.drawImage(image,x,y,gp.tileSize,gp.tileSize,null);
+    }
 }
