2e022db8644382d66b4f1fe8bd13ec9f099c4d6e
DarkStarBen <gall.27ben@gmail.com>
2026-05-06 23:20:04 +0200

    Spostato PauseMenu nella sua classe. Main Menu button ancora non è funzionante

--- src/main/java/uni/gaben/iscat/game/view/GameScene.java
+++ src/main/java/uni/gaben/iscat/game/view/GameScene.java
@@ -12,53 +12,21 @@ import uni.gaben.iscat.game.controller.GameController;
 /** Scena di gioco: posiziona il canvas e collega il controller. */
 public class GameScene extends Scene {
     private StackPane root;
-    private VBox pauseMenu;
-
-    private static final String CSS_MENU_BUTTON = "menu-button";
+    private final PauseMenu pauseMenu;
 
     public GameScene(GameController controller, GameCanvas canvas) {
         super(new StackPane());
         this.root = (StackPane) getRoot();
-        root.getChildren().add(canvas);
 
-        createPauseMenu(controller);
-        controller.setOnPauseToggle(isPaused -> showPauseMenu(isPaused));
-
-        controller.attachInput(this);
-        controller.startLoop();
-    }
-
-    public void showPauseMenu(boolean show) {
-        pauseMenu.setVisible(show);
-        pauseMenu.setMouseTransparent(!show);
-    }
-
-    private void createPauseMenu(GameController controller) {
-        pauseMenu = new VBox(20);
-        pauseMenu.setAlignment(Pos.CENTER);
-        pauseMenu.setStyle("-fx-background-color: rgba(0, 0, 0, 0.7);"); // Sfondo semi-trasparente
-
-        Label pauseLabel = new Label("GAME IS NOW PAUSED");
-        pauseLabel.setStyle("-fx-text-fill: white; -fx-font-size: 40px;");
-
-        Button resumeBtn = new Button("RIPRENDI");
-        resumeBtn.getStyleClass().add(CSS_MENU_BUTTON);
-        resumeBtn.setOnAction(e -> controller.togglePause()); // Chiamerà un metodo nel controller
-
-        Button mainMenuBtn = new Button("MAIN MENU");
-        mainMenuBtn.getStyleClass().add(CSS_MENU_BUTTON);
-        mainMenuBtn.setOnAction(e -> controller.togglePause());
-
-        Button quitBtn = new Button("QUIT");
-        quitBtn.getStyleClass().add(CSS_MENU_BUTTON);
-        quitBtn.setOnAction(e -> Platform.exit());
-
-        pauseMenu.getChildren().addAll(pauseLabel, resumeBtn, mainMenuBtn, quitBtn);
-        pauseMenu.setVisible(false); // Nascosto all'inizio
+        this.pauseMenu = new PauseMenu(controller);
+        root.getChildren().addAll(canvas, pauseMenu);
 
         String css = getClass().getResource("/uni/gaben/iscat/styles/menu.css").toExternalForm();
         getStylesheets().add(css);
 
-        root.getChildren().add(pauseMenu);
+        controller.setOnPauseToggle(pauseMenu::show);
+        controller.attachInput(this);
+        controller.startLoop();
     }
+
 }
---
+++ src/main/java/uni/gaben/iscat/game/view/PauseMenu.java
@@ -0,0 +1,52 @@
+package uni.gaben.iscat.game.view;
+
+import javafx.application.Platform;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import uni.gaben.iscat.game.controller.GameController;
+
+public class PauseMenu extends VBox {
+
+    private static final String CSS_MENU_BUTTON = "menu-button";
+
+    public PauseMenu(GameController controller) {
+        // Configurazione layout
+        setSpacing(20);
+        setAlignment(Pos.CENTER);
+        setStyle("-fx-background-color: rgba(0, 0, 0, 0.7);");
+
+        // Per coprire l'intero schermo
+        setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+
+        // Componenti
+        Label pauseLabel = new Label("GAME IS NOW PAUSED");
+        pauseLabel.setStyle("-fx-text-fill: white; -fx-font-size: 40px;");
+
+        Button resumeBtn = new Button("RIPRENDI");
+        resumeBtn.getStyleClass().add(CSS_MENU_BUTTON);
+        resumeBtn.setOnAction(e -> controller.togglePause());
+
+        Button mainMenuBtn = new Button("MAIN MENU");
+        mainMenuBtn.getStyleClass().add(CSS_MENU_BUTTON);
+
+        mainMenuBtn.setOnAction(e -> controller.togglePause());
+
+        Button quitBtn = new Button("QUIT");
+        quitBtn.getStyleClass().add(CSS_MENU_BUTTON);
+        quitBtn.setOnAction(e -> Platform.exit());
+
+        getChildren().addAll(pauseLabel, resumeBtn, mainMenuBtn, quitBtn);
+
+        // Inizialmente invisibile e trasparente ai click
+        setVisible(false);
+        setMouseTransparent(true);
+    }
+
+    public void show(boolean show) {
+        this.setVisible(show);
+        this.setMouseTransparent(!show);
+        if (show) this.toFront();
+    }
+}
