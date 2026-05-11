c1b05dee2968342c4d83b989a44022f80a9430f7
DarkStarBen <gall.27ben@gmail.com>
2026-05-11 20:59:37 +0200

    SkinMenu Parte 6 - Ora l'immagine di preview si aggiorna

--- src/main/java/uni/gaben/iscat/skin_menu/SkinMenuController.java
+++ src/main/java/uni/gaben/iscat/skin_menu/SkinMenuController.java
@@ -3,66 +3,86 @@ package uni.gaben.iscat.skin_menu;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import uni.gaben.iscat.IscatNavigator;
 import uni.gaben.iscat.IscatScenes;
 import uni.gaben.iscat.IscatSettings;
 import uni.gaben.iscat.gamenex.view.GamenexScene;
 
+import java.util.Objects;
+
 public class SkinMenuController {
 
+    @FXML private ImageView skinPreview;
     private String selectedSkinId;
+    private String selectedSkinPath;
 
     @FXML
     private void handleSkinSelect(ActionEvent event) {
         Button btn = (Button) event.getSource();
-        // TODO: cambia l'immagine con space goblin con la skin selezionata
         selectedSkinId = btn.getId();
+        updatePreview();
+    }
+
+    private void updatePreview() {
+        if (skinPreview == null) return;
+
+        String path = switch (selectedSkinId) {
+            case "skin1" -> "/uni/gaben/iscat/sprites/player1.png";
+            case "skin2" -> "/uni/gaben/iscat/sprites/player2.png";
+            case "skin3" -> "/uni/gaben/iscat/sprites/player3.png";
+            case "skin4" -> "/uni/gaben/iscat/sprites/player4.png";
+            case "skin5" -> "/uni/gaben/iscat/sprites/player5.png";
+            case "skin6" -> "/uni/gaben/iscat/sprites/player6.png";
+            case "skin7" -> "/uni/gaben/iscat/sprites/player7.png";
+            case "skin8" -> "/uni/gaben/iscat/sprites/player8.png";
+            case "skin9" -> "/uni/gaben/iscat/sprites/player9.png";
+            default -> "/uni/gaben/iscat/sprites/player1.png";
+        };
+
+        selectedSkinPath = path;
+
+        try {
+            Image newImage = new Image(Objects.requireNonNull(getClass().getResourceAsStream(path)));
+            skinPreview.setImage(newImage);
+        } catch (Exception e) {
+            System.err.println("Errore caricamento preview: " + path);
+        }
     }
 
     @FXML
     private void handleConfirm(ActionEvent event) {
-        if (selectedSkinId == null) {
+        if (selectedSkinPath == null) {
             System.out.println("Nessuna skin selezionata!");
             return;
         }
-        switch (selectedSkinId) {
-            case "skin1" -> IscatSettings.player_skin = "/uni/gaben/iscat/sprites/player1.png";
-            case "skin2" -> IscatSettings.player_skin = "/uni/gaben/iscat/sprites/player2.png";
-            case "skin3" -> IscatSettings.player_skin = "/uni/gaben/iscat/sprites/player3.png";
-            case "skin4" -> IscatSettings.player_skin = "/uni/gaben/iscat/sprites/player4.png";
-            case "skin5" -> IscatSettings.player_skin = "/uni/gaben/iscat/sprites/player5.png";
-            case "skin6" -> IscatSettings.player_skin = "/uni/gaben/iscat/sprites/player6.png";
-            case "skin7" -> IscatSettings.player_skin = "/uni/gaben/iscat/sprites/player7.png";
-            case "skin8" -> IscatSettings.player_skin = "/uni/gaben/iscat/sprites/player8.png";
-            case "skin9" -> IscatSettings.player_skin = "/uni/gaben/iscat/sprites/player9.png";
-        }
 
+        IscatSettings.player_skin = selectedSkinPath;
+
+        // Ricarica sprite nel gioco
         reloadPlayerSkin();
 
-        System.out.println("Skin cambiata in: " + IscatSettings.player_skin);
+        System.out.println("Skin confermata: " + selectedSkinPath);
         IscatNavigator.getInstance().navigateTo(IscatScenes.MENU);
-
     }
+
 
     @FXML
     private void handleBack(ActionEvent event) {
-        System.out.println("Ritorno al menu...");
         IscatNavigator.getInstance().navigateTo(IscatScenes.MENU);
     }
 
     private void reloadPlayerSkin() {
         try {
-            // Recupera la scena di gioco e ricarica lo sprite
             GamenexScene gameScene = (GamenexScene) IscatNavigator.getInstance()
                     .getScene(IscatScenes.GAMEN);
 
             if (gameScene != null) {
                 gameScene.reloadPlayerSkin();
-            } else {
-                System.err.println("GameScene non trovata");
             }
         } catch (Exception e) {
-            System.err.println("Errore durante il cambio skin: " + e.getMessage());
+            System.err.println("Errore reload sprite gioco: " + e.getMessage());
         }
     }
 }
