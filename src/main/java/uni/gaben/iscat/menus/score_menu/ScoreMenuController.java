7f6f08b242f434f6cfccf0fc57391fdb95a87527
DarkStarBen <gall.27ben@gmail.com>
2026-05-12 18:06:44 +0200

    Animazione Transiction quando si esce da ScoreMenu, [DA MIGLIORARE]

--- src/main/java/uni/gaben/iscat/menus/score_menu/ScoreMenuController.java
+++ src/main/java/uni/gaben/iscat/menus/score_menu/ScoreMenuController.java
@@ -1,10 +1,16 @@
 package uni.gaben.iscat.menus.score_menu;
 
+import javafx.animation.FadeTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
+import javafx.util.Duration;
 import uni.gaben.iscat.IscatNavigator;
 import uni.gaben.iscat.IscatScenes;
+
+import java.util.Objects;
 
 public class ScoreMenuController {
 
@@ -12,6 +18,26 @@ public class ScoreMenuController {
 
     @FXML
     private void handleBack(ActionEvent event) {
-        IscatNavigator.getInstance().navigateTo(IscatScenes.MAIN_MENU);
+        ImageView blackOverlay = new ImageView(
+                new Image(
+                        Objects.requireNonNull(
+                                getClass().getResourceAsStream("/uni/gaben/iscat/sprites/black.png")
+                        )
+                )
+        );
+        blackOverlay.setFitWidth(5000);
+        blackOverlay.setFitHeight(5000);
+
+        blackOverlay.setOpacity(0);
+        rootPane.getChildren().add(blackOverlay);
+
+        FadeTransition fadeIn = new FadeTransition(Duration.seconds(2),blackOverlay);
+        fadeIn.setFromValue(0.0);
+        fadeIn.setToValue(1.0);
+        fadeIn.play();
+
+        fadeIn.setOnFinished(e ->
+                IscatNavigator.getInstance().navigateTo(IscatScenes.MAIN_MENU)
+        );
     }
 }
