8f0e46baf3af58eee75ef3c851f299bbc32f5001
g0z <oqk.gabrielecircosta@gmail.com>
2026-05-31 21:37:54 +0200

    hotfix angle

--- src/main/java/uni/gaben/iscat/universe/rendering/RenderingSettings.java
+++ src/main/java/uni/gaben/iscat/universe/rendering/RenderingSettings.java
@@ -5,7 +5,7 @@ public class RenderingSettings {
         /* This utility class should not be instantiated */
     }
 
-    public static final double BASE_ROTDEG_OFFSET = 90.0;
+    public static final double BASE_ROTDEG_OFFSET = 270.0;
 
     public static final double BASE_ROTRAD_OFFSET = Math.toRadians(BASE_ROTDEG_OFFSET);
 }
